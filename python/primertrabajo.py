eb5b0f417761635d7fe2de8313e96afe99d14223
Duban Londoño <dubanl92@gmail.com>
2021-07-14 21:15:59 +0000

    Se sube el archivo con el primer trabajo que se debe entregar de Python

--- python/primertrabajo.py
+++ python/primertrabajo.py
@@ -1,75 +1,54 @@
-###Usted hace parte del equipo de censo del gobierno nacional, se le ha pedido crear un
-#formulario con las siguientes preguntas, el formulario debe permitir que la persona
-#especifique sus datos y luego mostrar en pantalla las respuestas dadas. Habrá
-#preguntas que requerirán operaciones por lo que se deberá almacenar y procesar
-#los datos involucrados. A continuación, se presentan las preguntas que debe
-#contener el formulario, el tipo de dato en él se deben almacenar y si tienen alguna
-#operación luego de leer la variable.
-
-dic_vacio["nombre"] = "luis"
-dic_vacio["apellido"] = "vasquez"
-
-print(dic_vacio)
-# diccionarios de primer nivel
-print(dic_vacio["nombre"])
-
-
 nombre = str(input("Primer nombre: "))
-apellido= str(input("Apellido: "))
-edad= int(input("Edad: "))
-barrio= str(input("Barrio: "))
-salariomensual= float(input("Salario mensual: "))
-salarioanual=salariomensual*12
-alturacm= float(input("Altura en cm: "))
-alturam=alturacm/100
-print( "Para el estado civil escoger una de las siguientes opciones")
-estadocivil= str(input("Estado civil: "))
-print(f"La diferencia entre ellos es {numero2 - numero1}.")
-nombre
+print (len(nombre) - nombre.count(' '))
 
-Preguntas:
-● Primer nombre: de tipo texto.
-● Primer apellido: de tipo texto.
-● Edad: de tipo entero.
-● Barrio: tipo texto
-● Salario mensual deseado: tipo flotante, luego de leer y almacenar el valor se
-debe multiplicar por 12 y almacenar en una nueva variable llamada
-salario_anual.
-● Estatura (en centímetros): tipo flotante, luego de leer y almacenar se debe
-convertir a metros y almacenar en una variable llamada estatura_mt
-● Estado civil: tipo texto, se debe sugerir en la pregunta que se introduzca una
-de las siguientes opciones: Casad@, Divorciad@, Viud@, Solter@, Union
-libre, de la respuesta sólo se debe almacenar las 3 primeras letras
-● Tiempo destinado estudio curso Python en horas a la semana: tipo entero.
-Luego de almacenar este dato se debe multiplicar por 52 y almacenar como
-estudio_python_anual
-● Cantidad de hij@s: Tipo entero.
-● Presupuesto salidas fines de semana: Tipo flotante. Este valor debe
-almacenarse y luego se debe multiplicar por 52 y guardar en la variable
-salidas_fines_semana
-● Computadores en casa: tipo entero
-● Nombre mejor amig@: tipo string.
-Valores adicionales para calcular:
-● Longitud total de caracteres para todas las respuestas,
-● Promedio cantidad caracteres por respuesta.
-● Concatenar todas las respuestas tipo texto, esto se hace acumulando las
-respuestas tipo texto. Luego medir y almacenar la longitud del texto
-concatenado.
-● Dividir por 3 la longitud total del texto concatenado, esto dará un índice
-para traer por partes todo el texto. Imprimir la primera parte, luego
-imprimir la parte del medio y finalmente, la tercera parte.
-Impresión resumen:
-Se espera que con las variables almacenadas arriba se impriman los siguientes
-párrafos, note que cada variable se pone entre símbolos de comparación:
-<nombre_mejor_amigo>
-● La persona encuentada se llama <primer_nombre> <primer_apellido>.
-Tiene <edad> años
-● Vive en <barrio> y está <estado_civil> para l@s interesed@s.
-● Mide <estatura_mt> y tiene <cantidad_hijos>
-● Su meta de estudio debe ser <estudio_python_anual> horas al año
-● Desea ganar <salario_anual> al año pero se gasta al año en ocio
-<salidas_fines_semana>
-● Va a instalar Python en <computadores_casa> computadores para estudiar
-4. Pruebas
-Una vez realizado el formulario, llenar y pedir a un compañero que intercambie de
-formulario. Cada pareja validará el correcto funcionamiento del formulario
+apellido= str(input("Apellido: "))
+print (len(apellido) - apellido.count(' '))
+
+edad= int(input("Edad: "))
+print (len(str(edad)))
+
+barrio= str(input("Barrio: "))
+print (len(barrio) - barrio.count(' '))
+
+salariomensual= int(input("Salario mensual: "))
+print (len(str(salariomensual)))
+
+salario_anual=salariomensual*12
+alturacm= int(input("Altura en cm: "))
+print (len(str(alturacm)))
+
+alturam=alturacm/100
+print("Escoger entre las siguientes opciones:")
+
+estado_civil= str(input("Casad@, Divorciad@, Viud@, Solter@, Union libre: "))
+print (len(estado_civil) - apellido.count(' '))
+
+tiempocurso = int(input("Tiempo destinado estudio curso Python en horas a la semana: "))
+print (len(str(tiempocurso)))
+estudio_python_anual = tiempocurso * 52
+
+hijos = int(input("Cantidad de hijos: "))
+print (len(str(hijos)))
+
+presupuestofinde = float(input("Presupuesto fin de semana: "))
+print (len(str(presupuestofinde)))
+
+salidas_fines_semana = presupuestofinde * 52
+computadores_casa = int(input("Numero de computadores en la casa: "))
+print (len(str(presupuestofinde)))
+
+nombremejoramigo = str(input("Nombre del mejor amig@: "))
+print (len(nombremejoramigo) - nombremejoramigo.count(' '))
+
+concatenar = nombre + apellido + barrio + estado_civil + nombremejoramigo
+sumaconcatenar = len(concatenar) - concatenar.count (' ')
+indice= int(sumaconcatenar/3)
+print(" La suma de Concatenar es " + str(sumaconcatenar))
+
+print("La persona encuestada se llama " + nombre.lower.capitalize() + " " + apellido.lower.capitalize())
+print("Vive en " + barrio.title() + " y esta " + estado_civil.capitalize() + " para l@s interesed@s")
+print("La persona mide "+ str(alturam) + " m y tiene " + str(hijos) + " hijos")
+print("Su meta de estudio debe ser " + str(estudio_python_anual) + " horas al año")
+print("Desea ganar " + str(salario_anual) + " al año pero se gasta al año en ocio " + str(salidas_fines_semana))
+print("Va a instalar python en " + str(computadores_casa) + " computadores para estudiar")
+
