21f6bf0a12b9a1cf32fd4f893b0e82712f7c4400
Duban Londoño <dubanl92@gmail.com>
2021-07-20 20:05:46 +0000

    Se agregan los cambios realizados

--- finalbash/final.sh
+++ finalbash/final.sh
@@ -21,57 +21,35 @@ echo Orbit: Moderniza flota de satelites
 echo Colony: Para colonizar la luna
 echo Mars: Realizar turismo en Marte
 
+###### GENERO VARIABLES PARA LAS FECHAS ############
 DIA1=`date +"%d-%m-%Y"`
 DIA2=''
-BK="bkmissions-`date +"%d%m%Y"`"
+BK="bkmissions-`date +"%d%m%Y"`" 
 
+###### AGREGO ESTA VARIABLE POR QUE EN CLOUD 9 NO EXISTE LA RUTA ###########
+RUTA=environment/pythoncourse/finalbash
+HOME=${HOME}/${RUTA}
+
+###### GENERO VARIABLES CON LAS RUTAS QUE SE VAN A CREAR #################
 RUTA1=${HOME}/takeoff-mission/devices/${DIA1}
+RUTA2=${HOME}/takeoff-mission/logsapolo11/${DIA1}
 
-RUTA2=${HOME}/takeoff-mission/logsapolo11/${BK}
-
-
-
-
+###### VARIABLE QUE TIENE EL VALOR DE LOS ESTADOS ##################
 estados=(UNKNOW EXCELENT GOOD WARNING KILL)
 
+mkdir -p ${HOME}/takeoff-mission/{devices,logsapolo11,config,stats} #2> /dev/null
+mkdir -p ${HOME}/takeoff-mission/devices/{$DIA1},${BK} #2> /dev/null
+mkdir -p ${HOME}/takeoff-mission/logsapolo11/${DIA1}
 
-if [ "$DIA1" != "$DIA2" ]; then
+sudo groupadd nasa 2>> ${RUTA2}/error.log
+sudo useradd -G nasa orbit 2>> ${RUTA2}/error.log
+sudo useradd -G nasa colony 2>> ${RUTA2}/error.log
+sudo useradd -G nasa mars 2>> ${RUTA2}/error.log
+#sudo useradd -G nasa unknow 2> ${RUTA2}/error.log
+
+echo $DIA1, $DIA2, $BK, $RUTA, $HOME, $RUTA1, $RUTA2,$estados
+
+#if [ "$DIA1" != "$DIA2" ]; then
 
 
-mkdir -p ${HOME}/takeoff-mission/devices/${DIA1},logsapolo11/${BK},config,stats} 2> /dev/null
-
-
-echo prueba > $RUTA2/error.log
-touch $RUTA2/error.log
-
-sudo groupadd nasa 2> ${RUTA2}/error.log
-sudo useradd -G nasa orbit 2> ${RUTA2}/error.log
-sudo useradd -G nasa colony 2> ${RUTA2}/error.log
-sudo useradd -G nasa mars 2> ${RUTA2}/error.log
-sudo useradd -G nasa unknow 2> ${RUTA2}/error.log
-
-NUM=1
-	while [ $NUM -lt 3 ]; do    
-	
-	       echo ${estados[$(($RANDOM%5))]} > ${RUTA1}/APL-ORB-000$NUM.log
-        echo ${estados[$(($RANDOM%5))]} > ${RUTA1}/APL-MRS-000$NUM.log
-        echo ${estados[$(($RANDOM%5))]} > ${RUTA1}/APL-CNL-000$NUM.log
-        echo ${estados[$(($RANDOM%5))]} > ${RUTA1}/APL-UNK-000$NUM.log
-        
-        chmod 777 ${RUTA1}/APL-ORB-000$NUM.log 2> ${RUTA2}/error.log
-        chmod 777 ${RUTA1}/APL-MRS-000$NUM.log 2> ${RUTA2}/error.log
-        chmod 777 ${RUTA1}/APL-CNL-000$NUM.log 2> ${RUTA2}/error.log
-        chmod 777 ${RUTA1}/APL-UNK-000$NUM.log 2> ${RUTA2}/error.log
-        
-        sudo chown orbit:nasa  ${RUTA1}/APL-ORB-000$NUM.log 2> ${RUTA2}/error.log
-        sudo chown mars:nasa   ${RUTA1}/APL-MRS-000$NUM.log 2> ${RUTA2}/error.log
-        sudo chown colony:nasa ${RUTA1}/APL-CNL-000$NUM.log 2> ${RUTA2}/error.log
-        sudo chown unknow:nasa ${RUTA1}/APL-UNK-000$NUM.log 2> ${RUTA2}/error.log
-
-        
-    let NUM=$NUM+1
-    DIA2=`date +"%d-%m-%Y"`
-	done
-else
- echo 'Felicidades ya se ejecuto el proceso de Apolo 11'
-fi
+#fi
