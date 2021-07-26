77a24c96804dfe747a0507359630012aab172a64
Duban Londoño <dubanl92@gmail.com>
2021-07-26 02:55:00 +0000

    Final

--- finalpython/app.py
+++ finalpython/app.py
@@ -2,7 +2,6 @@ import src.logica.stats as stats
 
 ##### DEFICIENCIA NO TENGO LA FORMA DE TOMAR LOS DATOS DEL DIA AUTOMATICAMENTE #####
 prueba=stats.fn_ejecutar_comando('sh $HOME/environment/pythoncourse/finalbash/final.sh')
-print(prueba)
 datos = stats.fn_ejecutar_comando('ls $HOME/environment/takeoff-mission/logsapolo11/23-07-2021')
 stats.fn_ejecutar_comando('touch $HOME/environment/takeoff-mission/stats/APLSTATS-23-07-2021.log')
 
@@ -10,6 +9,6 @@ lista_elemento = str(datos).split("\\n")
 lista_elemento=stats.fn_arreglar_lista(lista_elemento)
 salida=stats.fn_generar_dict(lista_elemento)
 
-#stats.resultados_proyecto(salida)
-#stats.resultados_estado(salida)
-#stats.resultados(salida)
+stats.resultados_proyecto(salida)
+stats.resultados_estado(salida)
+stats.resultados(salida)
--- finalpython/src/logica/stats.py
+++ finalpython/src/logica/stats.py
@@ -13,6 +13,18 @@ def fn_arreglar_lista(lista_elemento):
     lista_elemento.append(primer_valor)     #### Agrego la variable primer_valor como el ultimo elemento de la lista
     return lista_elemento
 
+def fn_generar_dict(lista_elementos):  #GENERAR UN DICCIONARIO A PARTIR DE LISTA DE ELEMENTOS EN LA CARPETA
+    documentos = {}
+    for element in lista_elementos:
+        try:
+            f = open(str(element),'r')              #####ELEMENTO ES EL NOMBRE DEL ARCHIVO, ABRE ESE ARCHIVO Y GUARDA EL VALOR EN F
+        except Exception as ex:
+            print ('No se encuentra el archivo')
+        else:
+            elem_temp = f.read()             #LEE EL VALOR DE  F y lo GUARDO EN ELEMETO TEMPORAL
+            documentos[element] = elem_temp[:-1]            #GENERO UN DICCIONARIO DONDE LA LLAVE ES EL NOMBRE DEL ARCHIVO Y EL VALOR ES EL VALOR QUE ESTA DENTRO DEL ARCHIVO
+    return documentos
+
 def generar_lista_keys (proyecto,diccionario):
     new_lista=[]  #GENERAR NUEVO DICCIONARIO A PARTIR DE UN DICCIONARIO EXISTENTE TENIENDO EN CUENTA EL VALOR DE UNA VARIABLE
     for k,v in diccionario.items():
@@ -50,15 +62,3 @@ def resultados(diccionario):
     for x in proyecto:      #### SE REALIZA UN RECORRIDO POR PROYECTO Y LUEGO POR ESTADO PARA ENCONTRAR LOS VALORES
         for y in estado:
             print("Se ha generado {} del proyecto: {}, con el estado: {}".format(len(buscar_proyecto_estado(x,y,diccionario)),x,y))
-
-def fn_generar_dict(lista_elementos):  #GENERAR UN DICCIONARIO A PARTIR DE LISTA DE ELEMENTOS EN LA CARPETA
-    documentos = {}
-    for element in lista_elementos:
-        try:
-            f = open(str(element),'r')              #####ELEMENTO ES EL NOMBRE DEL ARCHIVO, ABRE ESE ARCHIVO Y GUARDA EL VALOR EN F
-        except Exception as ex:
-            print ('No se encuentra el archivo')
-        else:
-            elem_temp = f.read()             #LEE EL VALOR DE  F y lo GUARDO EN ELEMETO TEMPORAL
-            documentos[element] = elem_temp[:-1]            #GENERO UN DICCIONARIO DONDE LA LLAVE ES EL NOMBRE DEL ARCHIVO Y EL VALOR ES EL VALOR QUE ESTA DENTRO DEL ARCHIVO
-    return documentos
